404fa294e4a6947beacf1eb033acc163a22ca0ed
V1zav <valikzav2002@gmail.com>
2024-06-11 00:06:05 +0300

    lab1 done

---
+++ myproject/myapp/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='customuser_set',  # змінено з 'customuser_set' на 'customuser_set'
+        related_query_name='customuser',  # змінено з 'customuser' на 'customuser'
+        blank=True,
+        verbose_name='groups',
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='customuser_set',  # змінено з 'customuser_set' на 'customuser_set'
+        related_query_name='customuser',  # змінено з 'customuser' на 'customuser'
+        blank=True,
+        verbose_name='user permissions',
+        help_text='Specific permissions for this user.',
+    )
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=20)
+    address = models.TextField()
+
+    def __str__(self):
+        return self.name
